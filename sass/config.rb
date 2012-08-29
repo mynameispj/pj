43cb5371589a42d0ae27fc58851ab762889aca95
PJ McCormick <pjmccorm@amazon.com>
2012-08-28 19:41:58 -0700

    Ported from LESS to SASS/Compass and organized template files within easy-to-use folder structures

---
+++ sass/config.rb
@@ -0,0 +1,29 @@
+# Require any additional compass plugins here.
+#require "/Library/Ruby/Gems/1.8/gems/compass-rgbapng-0.1.1/lib/rgbapng/functions.rb"
+#require "/Library/Ruby/Gems/1.8/gems/compass-rgbapng-0.1.1/lib/rgbapng.rb"
+#require "/Library/Ruby/Gems/1.8/gems/compass-rgbapng-0.1.1/lib/stylesheets/_rgbapng.scss"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+output_style = :nested
+environment = :development
+
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
